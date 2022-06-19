e4a94870ca25710fbfbfe4faf18b8eff6ddb88d9
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-06-19 16:54:16 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+  function  submitData(name, email) {
+    const subData = {
+      name: "Steve",
+      email: "steve@steve.com",
+    };
+    
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(subData),
+    };
+    
+     fetch("http://localhost:3000/users", configurationObject)
+
+      .then((response) => {
+        return response.json();
+      })
+     .then(function (object) {
+      document.body.innerHTML = object["id"]
+     })
+
+     .catch( function (error){
+    document.body.innerHTML = error.message
+     })
+  }
